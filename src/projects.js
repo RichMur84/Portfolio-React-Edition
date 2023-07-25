7faf6bce92ff0385d08d85554d34ab9ed27fb1bf
Richard Murillo <grizzlylbc1@gmail.com>
2023-07-25 17:43:51 -0400

    fixed images

--- src/projects.js
+++ src/projects.js
@@ -5,7 +5,7 @@ export const projects = [
     description:
       "On this Module I created a mini quiz using HTML, CSS and Javascript The theme is Hollywood trivia and I Hope you enjoy it",
     imageLink:
-      "https://user-images.githubusercontent.com/122934157/741ff416-7b7e-47a0-be09-b8d51ec8cf8c.png",
+      "https://github.com/RichMur84/Portfolio-React-Edition/assets/122934157/74830005-d589-4da6-a969-d93d44f93b4b",
     liveLink: "https://richmur84.github.io/Are-You-Smarter-Than-/",
     gitHub: "https://github.com/RichMur84/Are-You-Smarter-Than-",
   },
@@ -14,7 +14,7 @@ export const projects = [
     description:
       "I Created a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.",
     imageLink:
-      "",
+      "https://github.com/RichMur84/Portfolio-React-Edition/assets/122934157/19242282-b5e5-42bf-996d-365949fa723f",
     liveLink: "https://richmur84.github.io/My-Work-Day-Scheduler/",
     gitHub: "https://github.com/RichMur84/My-Work-Day-Scheduler",
   },
@@ -23,7 +23,7 @@ export const projects = [
     description:
       "The application gives users a space to store created content via the dashboard page, comment on other users posts, as well as edit and delete their own posts.",
     imageLink:
-      "",
+      "https://github.com/RichMur84/Portfolio-React-Edition/assets/122934157/68e99866-dd36-4c97-be12-37c698979bcf",
     liveLink: "https://bloggin-roll-9ff400d7190b.herokuapp.com/",
     gitHub: "https://github.com/RichMur84/Bloggin-Roll",
   },
@@ -32,7 +32,7 @@ export const projects = [
     description:
       "A simple web-application where users can create safe and strong password for employees that provides greater security. The password generator takes in data from specific prompts that the user inputs and returns a password that fits within the criteria the user chooses. .",
     imageLink:
-      "",
+      "https://github.com/RichMur84/Portfolio-React-Edition/assets/122934157/a23d781e-81fa-491f-941b-4ad364fc69b1",
     liveLink: "https://richmur84.github.io/Password-Generator/",
     gitHub: "https://github.com/RichMur84/Password-Generator",
   },
@@ -41,7 +41,7 @@ export const projects = [
     description:
       "A weather dashboard application where users can search for current and future weather conditions of cities around the world.The site provides the user with the current day forecast as well as a 5 day forecast.",
     imageLink:
-      "",
+      "https://github.com/RichMur84/Portfolio-React-Edition/assets/122934157/b6f26de2-0079-4ee0-ab3d-ac3508df3c38",
     liveLink: "https://richmur84.github.io/Hows-the-Weather/",
     gitHub: "https://github.com/RichMur84/Hows-the-Weather",
   },
@@ -50,7 +50,7 @@ export const projects = [
     description:
       "The first website I designed and deployed to the worldwide web. My web development journey started right here!",
     imageLink:
-      "",
+      "https://github.com/RichMur84/Portfolio-React-Edition/assets/122934157/1026e7f4-71b2-4ac3-b877-31e59f014fde",
     liveLink: "https://richmur84.github.io/First-Module/",
     gitHub: "https://github.com/RichMur84/First-Module",
   },
